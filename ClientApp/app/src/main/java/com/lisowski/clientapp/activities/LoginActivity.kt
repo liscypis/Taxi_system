6ab7562282f9a96869a87ca3ba9724c44b10c0e2
liscypis <wojteklisowski95@gmail.com>
2020-08-12 00:27:56 +0200

    added  error catch from login request and added new font

--- ClientApp/app/src/main/java/com/lisowski/clientapp/activities/LoginActivity.kt
+++ ClientApp/app/src/main/java/com/lisowski/clientapp/activities/LoginActivity.kt
@@ -3,11 +3,18 @@ package com.lisowski.clientapp.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.lisowski.clientapp.API.ApiClient
 import com.lisowski.clientapp.R
 import com.lisowski.clientapp.Utils.SharedPreferencesManager
+import com.lisowski.clientapp.models.APIError
 import com.lisowski.clientapp.models.LoginRequest
 import com.lisowski.clientapp.models.LoginResponse
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.Callback
@@ -24,22 +31,65 @@ class LoginActivity : AppCompatActivity() {
         apiClient = ApiClient()
         sessionManager = SharedPreferencesManager(this)
 
-        apiClient.getApiService().login(LoginRequest("nowy6","haslo"))
-            .enqueue(object : Callback<LoginResponse> {
-                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                    Log.d(LOGIN_ACTIVITY, "onFailure: coś nie pykło ${t.message}" )
-                }
+        loginBnt.setOnClickListener {
 
-                override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
-                    val loginResponse = response.body()
+            val login = loginInput.editText?.text.toString().trim()
+            val password = passwordInput.editText?.text.toString().trim()
 
-                    if (loginResponse?.token != null) {
-                        sessionManager.saveAuthToken(loginResponse.token)
-                        Log.d(LOGIN_ACTIVITY, "onResponse: $loginResponse")
-                    } else {
-                        Log.d(LOGIN_ACTIVITY, "onResponse: Error")
+            if (login.isEmpty()) {
+                loginInput.error = "Podaj login"
+                loginInput.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                passwordInput.error = "Podaj hasło"
+                passwordInput.requestFocus()
+                return@setOnClickListener
+            }
+
+            apiClient.getApiService().login(LoginRequest(login, password))
+                .enqueue(object : Callback<LoginResponse> {
+                    override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                        Log.d(LOGIN_ACTIVITY, "onFailure: coś nie pykło ${t.message}")
                     }
-                }
-            })
+
+                    override fun onResponse(
+                        call: Call<LoginResponse>,
+                        response: Response<LoginResponse>
+                    ) {
+                        if (response.isSuccessful) {
+                            val loginResponse = response.body()
+                            if (loginResponse?.token != null) {
+                                sessionManager.saveAuthToken(loginResponse.token)
+                                Log.d(
+                                    LOGIN_ACTIVITY,
+                                    "onResponse success: ${loginResponse.toString()}"
+                                )
+                            }
+                        } else {
+                            val gson = Gson()
+                            val type = object : TypeToken<APIError>() {}.type
+                            val errorResponse: APIError =
+                                gson.fromJson(response.errorBody()?.charStream(), type)
+                            Log.d(LOGIN_ACTIVITY, "onResponse fail: ${errorResponse.toString()}")
+                            if (errorResponse.message == "Unauthorized")
+                                showLoginError()
+                        }
+
+                    }
+                })
+        }
+
+    }
+
+    private fun showLoginError() {
+        Toast.makeText(applicationContext, "Błędne dane", Toast.LENGTH_LONG)
+            .show()
+        errorTV.text = "Błędne dane"
+        passwordInput.error = " "
+        passwordInput.requestFocus()
+        loginInput.error = " "
+        loginInput.requestFocus()
     }
 }
